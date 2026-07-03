5f843ae1a0d28402a716c14bb59fb3a155a18c0f
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-07-03 15:29:36 +0300

    Revert 6059496

--- BitsAndBytes/run-bnb-quantization-lumi.sh
+++ BitsAndBytes/run-bnb-quantization-lumi.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-#SBATCH --account=project_xxxxxxxxx
+#SBATCH --account=project_xxxxxxx
 #SBATCH --partition=dev-g
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=7
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=32G
 #SBATCH --time=00:30:00
-
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
 
 # Load the module
 module purge
-module use /appl/local/laifs/modules
-module load lumi-aif-singularity-bindings
-
-# export path to used container image
-export SIF=/appl/local/laifs/containers/lumi-multitorch-u24r70f21m50t210-20260513_121430/lumi-multitorch-full-u24r70f21m50t210-20260513_121430.sif
+module use /appl/local/csc/modulefiles
+module load pytorch/2.7
 
 # This will store all the Hugging Face cache such as downloaded models
 # and datasets in the project's scratch folder
-export HF_HOME=/scratch/project_462001302/mmahnoor/llm-quantization-scripts/BitsAndBytes/hf-cache
+export HF_HOME=/scratch/${SLURM_JOB_ACCOUNT}/${USER}/hf-cache
 mkdir -p $HF_HOME
-export SINGULARITYENV_HF_HOME=$HF_HOME
 
-
-srun singularity exec -B /scratch/${SLURM_JOB_ACCOUNT}  --cleanenv "$SIF" bash -lc 'python3 bnb-quantization.py'
-
+srun python3 bnb-quantization.py
